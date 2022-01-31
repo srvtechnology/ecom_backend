c51897a722b0cf4e674e815a176b3f55a6871c98
jeet <jeetbasak54@gmail.com>
2022-01-31 12:18:57 +0530

    admin chng

--- app/Admin.php
+++ app/Admin.php
@@ -17,11 +17,9 @@ class Admin extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',''
+        'name', 'email', 'password',
     ];
 
-
-    
     /**
      * The attributes that should be hidden for arrays.
      *
